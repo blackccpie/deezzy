4a4bfe2a7bc6000950d4a1c81e3b34d795a8dbb5
blackccpie <blackccpie@free.fr>
2017-08-06 15:50:45 +0200

    - moved deezer wrapper to separate dir.

--- src/main.cpp
+++ src/main.cpp
@@ -22,7 +22,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 
-#include "deezer_wrapper.h"
+#include "deezer_wrapper/deezer_wrapper.h"
 
 #include <QtQml>
 #include <QQmlApplicationEngine>
