be1db11e889d5a56113e51b39a13ac6e2629d426
blackccpie <blackccpie@free.fr>
2017-08-08 20:23:28 +0200

    - cmake processor architecture detection.

--- cmake/FindDeezer.cmake
+++ cmake/FindDeezer.cmake
@@ -1,4 +1,25 @@
-# TODO : very basic and hardcoded for now :-(
-set(DEEZER_SDK_INSTALL_DIR "/home/blackccpie/Projets/deezer-native-sdk-v1.2.0")
+# The MIT License
+#
+# Copyright (c) 2017-2017 Albert Murienne
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+set(DEEZER_SDK_INSTALL_DIR "${CMAKE_SOURCE_DIR}/../deezer-native-sdk-v1.2.0")
 set(DEEZER_SDK_INCLUDE_DIR "${DEEZER_SDK_INSTALL_DIR}/SDK/Include")
-set(DEEZER_SDK_LIBRARY_DIR "${DEEZER_SDK_INSTALL_DIR}/SDK/Bins/Platforms/Linux/x86_64")
+set(DEEZER_SDK_LIBRARY_DIR "${DEEZER_SDK_INSTALL_DIR}/SDK/Bins/Platforms/Linux/${CMAKE_SYSTEM_PROCESSOR}")
